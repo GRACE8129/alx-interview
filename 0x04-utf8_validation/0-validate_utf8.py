eabef04e30938d9d69f4b3065f9de08e8a7065d6
osimen grace <osimenobehi6@gmail.com>
2024-08-24 15:55:30 +0100

    validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,23 +1,40 @@
 #!/usr/bin/python3
-"""UTF-8 Validation"""
+""" UTF-8 Validation """
 
 
 def validUTF8(data):
-    """method determines if given data set represents a valid UTF-8 encoding"""
-    num_bytes = 0
-    for num in data:
-        binary_rep = format(num, '#010b')[-8:]
-        if num_bytes == 0:
-            for bit in binary_rep:
-                if bit == '0':
-                    break
-                num_bytes += 1
-            if num_bytes == 0:
+    """
+    Method that determines if a given data set represents a valid
+    UTF-8 encoding.
+    """
+    number_bytes = 0
+
+    mask_1 = 1 << 7
+    mask_2 = 1 << 6
+
+    for i in data:
+
+        mask_byte = 1 << 7
+
+        if number_bytes == 0:
+
+            while mask_byte & i:
+                number_bytes += 1
+                mask_byte = mask_byte >> 1
+
+            if number_bytes == 0:
                 continue
-            if num_bytes == 1 or num_bytes > 4:
+
+            if number_bytes == 1 or number_bytes > 4:
                 return False
+
         else:
-            if not (binary_rep[0] == '1' and binary_rep[1] == '0'):
-                return False
-        num_bytes -= 1
-    return num_bytes == 0
+            if not (i & mask_1 and not (i & mask_2)):
+                    return False
+
+        number_bytes -= 1
+
+    if number_bytes == 0:
+        return True
+
+    return False
